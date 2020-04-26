d5403fa34ac7a4cdc9c6bc0fc45cde05ea81a2cf
Dmitry Belykh <dmitry.belykh@dataart.com>
2020-04-26 17:14:12 +0300

    Change project structure as it is required for the course final task submission.

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en", help="Choose browser language")
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope='function')
+def browser(request, browser_name=None):
+    user_language = request.config.getoption("language")
+    if browser_name is None:
+        browser_name = request.config.getoption("browser_name")
+    browser = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("intl.accept_languages", user_language)
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        assert browser_name == "chrome" or browser_name == "firefox", "You should choose browser: chrome or firefox"
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REPEAT_REGISTER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_SUBMIT_BUTTON).click()
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        index = self.browser.current_url.find('login')
+        assert index != -1, "Not a login page url (no 'login' in url)"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form id not presented"
